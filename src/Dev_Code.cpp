accce4f76cc0cf396f57af9a52f60dbf6f2212a6
Cpapman21 <chris@medisource.tech>
2025-04-05 17:47:43 -0700

    All Libaries

--- src/Dev_Code.cpp
+++ src/Dev_Code.cpp
@@ -9,9 +9,12 @@
 // Include Particle Device OS APIs
 #include "Particle.h"
 #include "Adafruit_ADXL343.h"
+#include "Adafruit_MPL3115A2.h"
 #include "Adafruit_Sensor.h"
 #include "PCF85063A.h"
 #include <Adafruit_BusIO_Register.h>
+#include "DFRobot_SHT20.h"
+#include "SensirionI2CSen5x.h"
 
 
 // Let Device OS manage the connection to the Particle Cloud
@@ -25,15 +28,9 @@ SYSTEM_THREAD(ENABLED);
 SerialLogHandler logHandler(LOG_LEVEL_INFO);
 Adafruit_ADXL343 accel = Adafruit_ADXL343(12345);
 // setup() runs once, when the device is first turned on
-
-#define INPUT_PIN_INT1   (5) // Uno = (2)
+#define INPUT_PIN_INT1   (D3) // Uno = (2)
 #define INPUT_PIN_INT2   (6) // Uno = (3)
 
-/**
- * This struct is used to count the number of times that specific interrutps
- * have been fired by the ADXL and detected on the MCU. They will increment
- * by one for each event associated with the specified interrupt 'bit'.
- */
 struct adxl_int_stats {
     uint32_t data_ready;
     uint32_t single_tap;
@@ -104,9 +101,9 @@ void config_interrupts(void)
   g_int_config_enabled.bits.watermark  = false;
   g_int_config_enabled.bits.freefall   = false;
   g_int_config_enabled.bits.inactivity = false;
-  g_int_config_enabled.bits.activity   = false;
+  g_int_config_enabled.bits.activity   = true;
   g_int_config_enabled.bits.double_tap = false;
-  g_int_config_enabled.bits.single_tap = true;
+  g_int_config_enabled.bits.single_tap = false;
   g_int_config_enabled.bits.data_ready = false;    /* Set to INT2 */
   accel.enableInterrupts(g_int_config_enabled);
 
@@ -123,6 +120,26 @@ void config_interrupts(void)
 }
 
 
+SensirionI2CSen5x sen5x;
+
+
+void printSerialNumber() {
+  uint16_t error;
+  char errorMessage[256];
+  unsigned char serialNumber[32];
+  uint8_t serialNumberSize = 32;
+
+  error = sen5x.getSerialNumber(serialNumber, serialNumberSize);
+  if (error) {
+      Serial.print("Error trying to execute getSerialNumber(): ");
+      errorToString(error, errorMessage, 256);
+      Serial.println(errorMessage);
+  } else {
+      Serial.print("SerialNumber:");
+      Serial.println((char*)serialNumber);
+  }
+}
+
 
 
 void setup(void)
@@ -130,6 +147,21 @@ void setup(void)
   Serial.begin(9600);
   while (!Serial);
   Serial.println("ADXL343 Interrupt Tester"); Serial.println("");
+
+  Particle.variable("Motion",g_ints_fired);
+
+  Wire.begin();
+
+  sen5x.begin(Wire);
+
+  uint16_t error;
+  char errorMessage[256];
+  error = sen5x.deviceReset();
+  if (error) {
+        Serial.print("Error trying to execute deviceReset(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+  }
 
   /* Initialise the sensor */
   if(!accel.begin())
@@ -150,13 +182,15 @@ void setup(void)
   config_interrupts();
 
   Serial.println("ADXL343 init complete. Waiting for INT activity.");
+
+  
 }
 
 void loop(void)
 {
   /* Get a new sensor event */
-  sensors_event_t event;
-  accel.getEvent(&event);
+  //sensors_event_t event;
+  //accel.getEvent(&event);
   delay(10);
 
   while (g_ints_fired) {
@@ -167,7 +201,7 @@ void loop(void)
       /* Decrement the unhandled int counter. */
       g_ints_fired--;
   }
+
+  printSerialNumber();
+
 }
-
-
-
