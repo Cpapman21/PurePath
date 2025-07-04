1fe4b9887af0aa1a5ded4a70ccc808ac964f0909
Cpapman21 <chris@medisource.tech>
2025-07-04 16:31:32 -0400

    Payload_Message

--- src/Dev_Code.cpp
+++ src/Dev_Code.cpp
@@ -5,10 +5,22 @@
 #include "Particle.h"
 #include "Smog_Dog_Sensors.h"
 #include "Motion_Detect.h"
+#include "SdFat.h"
+#include "stdio.h"
 
-//Smog_Dog_Sensors Sensor;
+SYSTEM_MODE(AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+
 Motion Accel_1;
-Smog_Dog_Sensors Sensors;
+Smog_Dog_Sensors  Sensors;
+Smog_Dog_Sensors* Sensors_P = &Sensors;
+
+SdFat sd;
+int chipSelect1 = A5;
+//void tryMeFirst();
+
 
 void setup()
 {
@@ -16,34 +28,36 @@ void setup()
   Serial.begin(115200);
   delay(1000);
   Wire.begin();
+  pinMode(D3,OUTPUT);
+  digitalWrite(D3,HIGH);
+  //tryMeFirst();
   Sensors.SE55_Initalize();
   Sensors.GPS_Initalize();
-
+  String name("Smog Dog");
 }
 
 void loop()
 {
   Sensors.S55_Data();
   Sensors.GPS_Data();
-  Serial.println(Sensors.ambientTemperature);
+  String payload = String::format("{\"temp\":%.1f,\"rh\":%.1f}", Sensors_P ->ambientTemperature , Sensors_P->ambientHumidity );
+  Particle.publish("Test Load",payload);
+  delay(2000);
+  
 }
 
 
 void tryMeFirst() {
-	//File myFile;
+	File myFile;
 
 	// Initialize the library
-	//if (!sd.begin(chipSelect1, SD_SCK_MHZ(4))) {
-		//Serial.println("failed to open card");
-		//return;
-	//}
-
-
-
-  
+	if (!sd.begin(chipSelect1, SD_SCK_MHZ(4))) {
+		Serial.println("failed to open card");
+		return;
+	}
 
 	// open the file for write at end like the "Native SD library"
-  /*
+  
 	if (!myFile.open("test.txt", O_RDWR | O_CREAT | O_AT_END)) {
 		Serial.println("opening test.txt for write failed");
 		return;
@@ -71,6 +85,6 @@ void tryMeFirst() {
 	}
 	// close the file:
 	myFile.close();
-  */
+  
 
 }
